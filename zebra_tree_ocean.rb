03a7a2624d23ac2067494971a5655f73e116f385
deon0608 <136635416+deon0608@users.noreply.github.com>
2023-07-19 16:05:09 +0700

    Improve error handling

---
+++ zebra_tree_ocean.rb
@@ -0,0 +1,40 @@
+# Edible Education in Ruby
+
+# 1. Create an Edible Education class
+class EdibleEducation
+	# 2. Define methods to detail class
+	def learn_edible_basics
+		puts "Learning the basics of edible education is the first step to becoming a confident foodie."
+	end
+
+	def start_cooking
+		puts "Start with simple recipes and expand your skillset as you build confidence in the kitchen."
+	end
+
+	def explore_cuisines
+		puts "Explore new cuisines, ingredients, and cooking techniques to broaden your culinary horizons."
+	end
+
+	def develop_healthy_habits
+		puts "Develop healthy eating habits by trying new recipes, planning meals, and understanding nutrition labels."
+	end
+
+	def plan_meals
+		puts "Plan meals and snacks to ensure you have adequate nutrition throughout the day."
+	end
+
+	def create_recipes
+		puts "Create your own recipes using your favorite flavors and ingredients and share them with others!"
+	end
+end
+
+# 3. Create an instance of the Edible Education class
+edible_education = EdibleEducation.new
+
+# 4. Call the methods of the edible_education instance
+edible_education.learn_edible_basics
+edible_education.start_cooking
+edible_education.explore_cuisines
+edible_education.develop_healthy_habits
+edible_education.plan_meals
+edible_education.create_recipes
